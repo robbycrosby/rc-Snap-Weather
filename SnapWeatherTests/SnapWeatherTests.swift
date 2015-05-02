5b9b3c55cf79702dec24e1c400e59a7bec11d462
Robert Crosby <Rob@Roberts-MacBook-Air.local>
2015-05-02 14:45:51 -0700

    Initial Commit

---
+++ SnapWeatherTests/SnapWeatherTests.swift
@@ -0,0 +1,36 @@
+//
+//  SnapWeatherTests.swift
+//  SnapWeatherTests
+//
+//  Created by Robert Crosby on 5/2/15.
+//  Copyright (c) 2015 Robert Crosby. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SnapWeatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
